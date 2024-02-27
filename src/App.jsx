ec02606b979726b0a270bdf5e555c4d4b9f4b221
Ranjan <vermaranjan9964@gmail.com>
2024-02-27 20:47:03 +0530

    updated projectss

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import About from "./components/about/About";
-import Project from "./components/pro_ject/project";
+import Project from "./components/projectss/project";
 
 function App() {
   return (
